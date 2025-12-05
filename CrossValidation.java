36af48479e4a59a4c14c10951e996785902305e3
RmaUriel <uhernandez11404@gmail.com>
2025-12-05 09:45:53 -0800

    added tp, fp, etc

--- CrossValidation.java
+++ CrossValidation.java
@@ -26,9 +26,11 @@ public class CrossValidation {
         data.shuffle(new Random(42));
         int totalSize = data.getSize();
         int foldSize = totalSize / numFolds;
+        int remainder = totalSize % numFolds;
 
         double totalAccuracy = 0.0;
 
+        double totalF1 = 0.0;
         for (int fold = 0; fold < numFolds; fold++) {
             ProjectMatrix trainSet = new ProjectMatrix();
             ProjectMatrix testSet = new ProjectMatrix();
@@ -47,20 +49,37 @@ public class CrossValidation {
             svm.fit(trainSet, labelCol);
 
             // Evaluate accuracy
-            int correct = 0;
+            int tp = 0;
+            int fp = 0;
+            int tn = 0;
+            int fn = 0;
+
             for (int i = 0; i < testSet.getSize(); i++) {
                 int predicted = svm.predictSurvived(testSet, i, labelCol);
                 int actual = (int) testSet.getVal(i, labelCol);
-                if (predicted == actual) {
-                    correct++;
-                }
-            }
+                if (predicted == 1 && actual == 1) tp++;
+                else if (predicted == 1 && actual == 0) fp++;
+                else if (predicted == 0 && actual == 0) tn++;
+                else if (predicted == 0 && actual == 1) fn++;
 
+            }
+            int correct = tp + tn;
             double accuracy = (double) correct / testSet.getSize();
-            System.out.printf("Fold %d accuracy: %.4f. Correct: %d. Total: %d\n", fold + 1, accuracy, correct, testSet.getSize());
+            double precision = (tp + fp > 0) ? (double) tp / (tp + fp) : 0.0;
+            double recall = (tp + fn > 0) ? (double) tp / (tp + fn) : 0.0;
+            double f1 = (precision + recall > 0) ? 2 * precision * recall / (precision + recall) : 0.0;
+
+
+
+            System.out.printf("Fold %d metrics:\n", fold + 1);
+            System.out.printf("Accuracy: %.4f | Precision: %.4f | Recall: %.4f | F1: %.4f\n", accuracy, precision, recall, f1);
+            System.out.printf("TP: %d, TN: %d, FP: %d, FN: %d, Total: %d\n", tp, tn, fp, fn, testSet.getSize());
+
             totalAccuracy += accuracy;
+            totalF1 += f1;
         }
 
-        System.out.printf("Average %d-fold CV accuracy: %.4f\n", numFolds, totalAccuracy / numFolds);
+        System.out.printf("\nAverage %d-fold CV metrics:\n", numFolds);
+        System.out.printf("Accuracy: %.4f | F1-score: %.4f\n", totalAccuracy / numFolds, totalF1 / numFolds);
     }
 }
